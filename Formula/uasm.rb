406b78458c945a6d4234da27772dfa8713e98665
Jupiter <795353+upiter@users.noreply.github.com>
2024-12-07 13:00:46 +0300

    Update formula after `brew audit`

--- Formula/uasm.rb
+++ Formula/uasm.rb
@@ -1,16 +1,16 @@
 # UASM
 class Uasm < Formula
-	desc "UASM - Macro Assembler"
-	homepage "https://github.com/upiter/uasmac"
-	version "2.57"
-	url "https://github.com/upiter/uasmac/releases/download/2.57/uasm-2.57-macos-x86_64.zip"
-	sha256 "6d772f5033d210aede1b8267f5e6282fb73cc60536b1a4113c5aade6873e0eba"
+  desc "Macro Assembler (MASM-compatible), based on JWasm"
+  homepage "https://github.com/upiter/uasmac"
+  url "https://github.com/upiter/uasmac/releases/download/2.57/uasm-2.57-macos-x86_64.zip"
+  version "2.57"
+  sha256 "6d772f5033d210aede1b8267f5e6282fb73cc60536b1a4113c5aade6873e0eba"
 
-	def install
-		bin.install "uasm"
-	end
+  def install
+    bin.install "uasm"
+  end
 
-	test do
-		system "#{bin}/uasm", "-h"
-	end
+  test do
+    system bin/"uasm", "-h"
+  end
 end
